88bc43d32955a2c8697f820e808d4491f89a695c
Omsai Kalekar <omsaikalekar2000@gmail.com>
2025-04-21 14:22:58 +0530

    lnitial commit

---
+++ debugProject/src/debugProject/calculator.java
@@ -0,0 +1,26 @@
+package debugProject;
+
+public class calculator {
+	void add(int n1, int n2) {
+		int sum = n1 + n2;
+		System.out.println("addition" + sum);
+	}
+
+	
+		void sub(int n1, int n2) {
+			int sub = n1 - n1;
+			System.out.println("Subtraction: " + sub);
+		}
+	
+ 
+	void mul(int n1, int n2) {
+		int mul = n2 * n2;
+		System.out.println("Multiplication: " + mul);
+	}
+
+	void div(int n1, int n2) {
+
+		int div = n1 / n2;
+		System.out.println("Division: " + div);
+	}
+}
---
+++ debugProject/src/debugProject/dubtest.java
@@ -0,0 +1,19 @@
+package debugProject;
+
+import debugProject.calculator;
+
+public class dubtest {
+	
+	public static void main(String args[])
+	{
+		calculator c1=new calculator();
+		c1.add(10,20);
+		c1.sub(10,20);
+		c1.mul(20,30);
+		c1.div(10,10);
+		
+		
+	}
+
+}
+   
